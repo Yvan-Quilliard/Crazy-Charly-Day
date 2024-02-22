2996edeaa366211d549b96134b4f1913a9c3c4ec
Yghore <Yhgore@gmail.com>
2024-02-22 17:00:06 +0100

    feat(seeder) : AtelierDemande, Refused

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Atelier;
 use App\Models\Theme;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
@@ -15,13 +17,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(20)->create();
+        \App\Models\User::factory(20)->create()->each(function ($user){
+
+        });
 
          \App\Models\User::factory()->create([
              'name' => 'admin',
              'email' => 'admin@example.com',
              'password' => Hash::make('password'),
-         ]);
+         ])->each(function ($user){
+             $atelierDemande = Atelier::factory()->create();
+             $user->demands2ateliers()->attach($atelierDemande);
+             $atelierRefused = Atelier::factory()->create();
+             $user->demandsRefused()->attach($atelierRefused);
+         });
 
         $this->call([
             ThemeSeeder::class,

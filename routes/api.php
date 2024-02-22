2ba750b17ad4229baadcacdab243e94b41f290d7
Yvan <yvanquilliard@hotmail.fr>
2024-02-22 14:20:39 +0100

    Fit routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,22 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('ateliers', 'App\Http\Controllers\AtelierController@index');
+Route::get('ateliers/{id}', 'App\Http\Controllers\AtelierController@show');
+Route::post('ateliers', 'App\Http\Controllers\AtelierController@store');
+Route::put('ateliers/{id}', 'App\Http\Controllers\AtelierController@update');
+Route::delete('ateliers/{id}', 'App\Http\Controllers\AtelierController@destroy');
+
+Route::get('users', 'App\Http\Controllers\UserController@index');
+Route::get('users/{id}', 'App\Http\Controllers\UserController@show');
+Route::post('users', 'App\Http\Controllers\UserController@store');
+Route::put('users/{id}', 'App\Http\Controllers\UserController@update');
+Route::delete('users/{id}', 'App\Http\Controllers\UserController@destroy');
+
+Route::get('themes', 'App\Http\Controllers\ThemeController@index');
+Route::get('themes/{id}', 'App\Http\Controllers\ThemeController@show');
+Route::post('themes', 'App\Http\Controllers\ThemeController@store');
+Route::put('themes/{id}', 'App\Http\Controllers\ThemeController@update');
+Route::delete('themes/{id}', 'App\Http\Controllers\ThemeController@destroy');
+

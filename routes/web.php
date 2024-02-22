0592953d1678f344ca448e5194d3a15ebb29e715
Yvan <yvanquilliard@hotmail.fr>
2024-02-22 16:09:19 +0100

    Fit: add test route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get("/test", function() {
+    dd(User::findOrFail(1)->with('demands2ateliers')->get());
 });
 
 Route::middleware([

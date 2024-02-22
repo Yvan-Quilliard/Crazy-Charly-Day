81ec1143b9f450bbba8a36ee4c0c145a35429a1a
Yvan <yvanquilliard@hotmail.fr>
2024-02-22 18:38:43 +0100

    Fix: fix namespace

--- app/Http/Requests/Theme/UpdateThemeRequest.php
+++ app/Http/Requests/Theme/UpdateThemeRequest.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Theme;
 
+use App\Http\Requests\FormRequestCustom;
 class UpdateThemeRequest extends FormRequestCustom
 {
     /**
--- app/Http/Requests/User/StoreUserRequest.php
+++ app/Http/Requests/User/StoreUserRequest.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\User;
+
+use App\Http\Requests\FormRequestCustom;
 
 class StoreUserRequest extends FormRequestCustom
 {
